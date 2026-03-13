473de93f6d7fab1c243255d06ce589b68603d62b
Marcello Alves <marcelloa.adm@gmail.com>
2026-03-12 21:01:34 -0300

    subindo c00/ex05

--- c/c00/ex05/ft_print_comb.c
+++ c/c00/ex05/ft_print_comb.c
@@ -3,16 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_comb.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marcelo <marcelo@student.42.fr>            +#+  +:+       +#+        */
+/*   By: maalzir- <maalzir-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 19:30:58 by maalzir-          #+#    #+#             */
-/*   Updated: 2026/03/12 13:02:44 by marcelo          ###   ########.fr       */
+/*   Updated: 2026/03/12 20:58:51 by maalzir-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_print(char a, char b, char c)
+{
+	ft_putchar(a);
+	ft_putchar(b);
+	ft_putchar(c);
+	if (!(a == '7' && b == '8' && c == '9'))
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
 void	ft_print_comb(void)
 {
-	write()
+	char	a;
+	char	b;
+	char	c;
+
+	a = '0';
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_print(a, b, c);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
 }
+//DESCOMENTE ABAIXO (SELECIONE E USE O 'CTRL + /') PARA TESTAR O PROGRAMA
+// int	main(void)
+// {
+// 	ft_print_comb();
+// 	return (0);
+// }

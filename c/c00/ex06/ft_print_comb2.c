0726c7449ae5ca3e5db25dc17aa9bd4ec8e88b6c
Marcello Alves <marcelloa.adm@gmail.com>
2026-03-14 21:22:00 -0300

    subindo c00/ex06 parcial

--- c/c00/ex06/ft_print_comb2.c
+++ c/c00/ex06/ft_print_comb2.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maalzir- <maalzir-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/11 19:30:58 by maalzir-          #+#    #+#             */
-/*   Updated: 2026/03/14 18:42:38 by maalzir-         ###   ########.fr       */
+/*   Created: 2026/03/12 21:06:30 by maalzir-          #+#    #+#             */
+/*   Updated: 2026/03/14 21:14:17 by maalzir-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,47 +14,53 @@
 
 void	ft_putchar(char c)
 {
-	write (1, &c, 1);
+	write(1, &c, 1);
 }
 
-void	ft_print(char a, char b, char c)
+void	ft_print_1(char a, char b)
 {
 	ft_putchar(a);
 	ft_putchar(b);
-	ft_putchar(c);
-	if (!(a == '7' && b == '8' && c == '9'))
+	
+	if (!(a == '9' && b == '8'))
 	{
 		ft_putchar(',');
 		ft_putchar(' ');
 	}
 }
 
-void	ft_print_comb(void)
+void	ft_print_2(char	c, char	d)
+{
+	ft_putchar(c);
+	ft_putchar(d);
+
+	if (!(c == '9' && d == '9'))
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb2(void)
 {
 	char	a;
 	char	b;
 	char	c;
+	char	d;
 
-	a = '0';
-	while (a <= '7')
+	d = '0';
+	while (d <= '9')
 	{
-		b = a + 1;
-		while (b <= '8')
+		while (c <= '9')
 		{
-			c = b + 1;
-			while (c <= '9')
-			{
-				ft_print(a, b, c);
-				c++;
-			}
-			b++;
+			
 		}
-		a++;
 	}
 }
+
 //DESCOMENTE ABAIXO (SELECIONE E USE O 'CTRL + /') PARA TESTAR O PROGRAMA
-// int	main(void)
-// {
-// 	ft_print_comb();
-// 	return (0);
-// }
+int	main(void)
+{
+	ft_print_comb2();
+	return (0);
+}

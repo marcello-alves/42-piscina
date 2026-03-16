ab42b27e946e39d00fc1e12c3a3c78d9dea00173
Marcello Alves <marcelloa.adm@gmail.com>
2026-03-15 21:56:46 -0300

    ex06 finalizado e inicio do ex07

--- c/c00/ex07/ft_putnbr.c
+++ c/c00/ex07/ft_putnbr.c
@@ -1,67 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maalzir- <maalzir-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/12 21:06:30 by maalzir-          #+#    #+#             */
-/*   Updated: 2026/03/15 20:37:31 by maalzir-         ###   ########.fr       */
+/*   Created: 2026/03/15 20:39:27 by maalzir-          #+#    #+#             */
+/*   Updated: 2026/03/15 21:55:29 by maalzir-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_print_nb(int n)
-{
-	char	d;
-
-	d = '0' + n / 10;
-	write(1, &d, 1);
-	d = '0' + n % 10;
-	write(1, &d, 1);
-}
-
-void	ft_print_sep(int a, int b)
+void	ft_putnbr(int nb)
 {
 	char	c;
 
-	if (!(a == 98 && b == 99))
+	if (nb < 0)
 	{
-		c = ',';
+		c = '-';
 		write(1, &c, 1);
-		c = ' ';
-		write(1, &c, 1);
-	}
-}
-
-void	ft_print_comb2(void)
-{
-	int		a;
-	int		b;
-	char	c;
-
-	a = 0;
-	b = a + 1;
-	while (a <= 98)
-	{
-		while (b <= 99)
-		{
-			ft_print_nb(a);
-			c = ' ';
-			write(1, &c, 1);
-			ft_print_nb(b);
-			ft_print_sep(a, b);
-			b++;
-		}
-	a++;
-	b = a + 1;
 	}
 }
 
 //DESCOMENTE ABAIXO (SELECIONE E USE O 'CTRL + /') PARA TESTAR O PROGRAMA
-// int	main(void)
+// int main()
 // {
-// 	ft_print_comb2();
-// 	return (0);
+// 	ft_putnbr(-1);
+// 	return(0);
 // }

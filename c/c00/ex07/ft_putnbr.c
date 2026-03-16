90c503be23b37571412e0b80a292727ca3f78baf
Marcello Alves <marcelloa.adm@gmail.com>
2026-03-16 13:57:14 -0300

    finalizados até ex07 da c00

--- c/c00/ex07/ft_putnbr.c
+++ c/c00/ex07/ft_putnbr.c
@@ -6,26 +6,50 @@
 /*   By: maalzir- <maalzir-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/15 20:39:27 by maalzir-          #+#    #+#             */
-/*   Updated: 2026/03/15 21:55:29 by maalzir-         ###   ########.fr       */
+/*   Updated: 2026/03/16 13:52:42 by maalzir-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_print(char c)
+{
+	write(1, &c, 1);
+}
+
 void	ft_putnbr(int nb)
 {
-	char	c;
+	long int	n;
 
-	if (nb < 0)
+	n = nb;
+	if (n < 0)
 	{
-		c = '-';
-		write(1, &c, 1);
+		ft_print('-');
+		n *= -1;
 	}
+	if (n >= 10)
+	{
+		ft_putnbr(n / 10);
+	}
+	ft_print(n % 10 + '0');
 }
 
 //DESCOMENTE ABAIXO (SELECIONE E USE O 'CTRL + /') PARA TESTAR O PROGRAMA
 // int main()
 // {
-// 	ft_putnbr(-1);
-// 	return(0);
+//     ft_putnbr(0);
+//     write(1, "\n", 1);
+//     ft_putnbr(42);
+//     write(1, "\n", 1);
+//     ft_putnbr(-42);
+//     write(1, "\n", 1);
+//     ft_putnbr(2147483647);
+//     write(1, "\n", 1);
+//     ft_putnbr(-2147483648);
+//     write(1, "\n", 1);
+//     ft_putnbr(1000000000);
+//     write(1, "\n", 1);
+//     ft_putnbr(-1);
+//     write(1, "\n", 1);
+//     return (0);
 // }
---
+++ testes/entendendo_ex06.c.save
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+int main(void)
+{
+	int n;
+	char c;
+
+	n = 7;
+	c = '0' + n / 10;
+	write(1, &c , 1);
+	c = '0' + n % 10;
+	write(1, &c, 1);
+	return(0);
+}

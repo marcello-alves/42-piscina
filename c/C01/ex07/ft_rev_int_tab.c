fa03c08c488d2f2bee0b60e1f16d4150aba34985
Marcello Alves <marcelloa.adm@gmail.com>
2026-03-18 16:29:32 -0300

    finalizando lista C01 - primeiro envio

--- c/C01/ex07/ft_rev_int_tab.c
+++ c/C01/ex07/ft_rev_int_tab.c
@@ -6,7 +6,39 @@
 /*   By: maalzir- <maalzir-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/17 22:46:43 by maalzir-          #+#    #+#             */
-/*   Updated: 2026/03/17 22:54:59 by maalzir-         ###   ########.fr       */
+/*   Updated: 2026/03/18 16:27:10 by maalzir-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_rev_int_tab(int	*tab, int size)
+{
+	int	i;
+	int	j;
+	int	tmp;
+
+	i = 0;
+	j = size - 1;
+	while (i < j)
+	{
+		tmp = tab[i];
+		tab[i] = tab[j];
+		tab[j] = tmp;
+		i++;
+		j--;
+	}
+}
+
+//DESCOMENTE ABAIXO (SELECIONE E USE O 'CTRL + /') PARA TESTAR O PROGRAMA
+// #include <stdio.h>
+// int	main()
+// {
+// 	int	i = 0;
+// 	int	tab[] = {1, 2, 3, 4, 5, 6, 7};
+// 	int size = sizeof(tab)/sizeof(tab[0]);
+// 	ft_rev_int_tab(tab, size);
+// 	while (i < size)
+// 	{
+// 		printf("%d ", tab[i]);
+// 		i++;
+// 	}
+// }
